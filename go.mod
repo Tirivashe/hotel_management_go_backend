36e46ef917b927c5acd7b9b59da6d42e95ff829f
Tirivashe <shaymusts@gmail.com>
2024-07-26 18:27:42 +0200

    Initializers
    Added initializer for env files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Tirivashe/hotel_management_backend
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "github.com/Tirivashe/hotel_management_backend/initializers"
+
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
 	println("Hello, world!")
 }
